d74be8bc6a407c038c5fada29432be061f154441
Sk7x <schilleranatole14@gmail.com>
2025-04-23 11:41:52 +0200

    PROBLEME PC ANAT PLEIN_ECRAN + TEST

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "affichage.h"
 #include <windows.h>
-
+#include "affichage.h"
+#include "donnee.h"
+#include "plateau.h"
 int main()
 {
+
     SetConsoleOutputCP(CP_UTF8);
     system("cls");
     plein_ecran();
     for (int i =0; i<=3; i++) {
         printf("%d\n", i);
     }
+    bateau bateau;
+    bateau.equipe_leader= -1;
+    bateau.pions[0].equipe=0;
+    bateau.pions[1].equipe=0;
+    bateau.pions[2].equipe=0;
+    int leader = determiner_leader(bateau);
+    printf("\n\n%d",leader);
     //contour(10, 10, 10, 10);
     getchar();
     return 0;
